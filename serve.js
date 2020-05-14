69af57f404ff430651d5381c1a52c7c1b213eff5
eric-robertson <e.robertson.js@gmail.com>
2020-05-14 19:01:58 -0400

    123

--- serve.js
+++ serve.js
@@ -1,4 +1,8 @@
 let express = require('express')
 let app = express()
+app.use('/', (req,res,next) => {
+    console.log('got Request');
+    next();
+})
 app.use('/', express.static('./build') )
 app.listen(80, () => console.log('online'));
